006ae482ddcef7e1477dbcd6b9ca1b2416fd04b3
Jabari <xuejianbao@foxmail.com>
2025-04-22 00:09:51 +0800

    update self-manitained ui, support printf
    Signed-off-by: Jabari <xuejianbao@foxmail.com>

--- 6_u8g2_oled/Core/Src/pages_demo.c
+++ 6_u8g2_oled/Core/Src/pages_demo.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "u8g2.h"
 #include "oled.h"
 #include "stm32f1xx_hal.h"
@@ -11,6 +13,45 @@ typedef struct MenuItem {
     void (*action)();
 } MenuItem;
 
+// 定义新的全局结构体
+typedef struct {
+    MenuItem *currentMenu;
+    MenuItem *parentMenu;
+    int currentIndex;
+} MenuContext;
+
+void action1();
+void action2();
+void actionBack();
+
+MenuItem submenu1[] = {
+    {"..", NULL, actionBack},
+    {"child menu1-1", NULL, action1},
+    {"child menu1-2", NULL, action2},
+    {NULL, NULL, NULL},
+};
+
+MenuItem submenu2[] = {
+    {"..", NULL, actionBack}, 
+    {"child menu2-1", NULL, action1},
+    {"child menu2-2", NULL, action2},
+    {NULL, NULL, NULL},
+};
+
+// 定义主菜单
+MenuItem mainMenu[] = {
+    {"main menu1", submenu1, NULL},
+    {"main menu2", submenu2, NULL},
+    {NULL, NULL, NULL},
+};
+
+// 初始化全局结构体
+MenuContext menuContext = {
+    .currentMenu = mainMenu,
+    .parentMenu = NULL,
+    .currentIndex = 0
+};
+
 // 示例菜单动作函数
 void action1() {
     // 实现具体动作
@@ -23,83 +64,84 @@ void action2() {
 }
 
 // 定义子菜单
-MenuItem submenu1[] = {
-    {"child menu1-1", NULL, action1},
-    {"child menu1-2", NULL, action2},
-    {NULL, NULL, NULL}
-};
+// 定义返回上一级的动作函数
+void actionBack() {
+    if (menuContext.parentMenu != NULL) {
+        menuContext.currentMenu = menuContext.parentMenu;
+        menuContext.currentIndex = 0;
+        // 父菜单返回后，其父菜单应置为 NULL，因为已回到上一级
+        menuContext.parentMenu = NULL;
+    }
+}
 
-MenuItem submenu2[] = {
-    {"child menu2-1", NULL, action1},
-    {"child menu2-2", NULL, action2},
-    {NULL, NULL, NULL}
-};
-
-// 定义主菜单
-MenuItem mainMenu[] = {
-    {"main menu1", submenu1, NULL},
-    {"main menu2", submenu2, NULL},
-    {NULL, NULL, NULL}
-};
-
-// 当前选中的菜单项指针
-MenuItem *currentMenu = mainMenu;
-int currentIndex = 0;
 
 // 绘制菜单
-void drawMenu(u8g2_t *u8g2) {
+// 定义阻尼系数
+#define DAMPING_FACTOR 0.5
+// 定义基础延迟时间
+#define BASE_DELAY 20
+
+// 绘制带动画过渡的菜单
+void drawAnimatedMenu(u8g2_t *u8g2) {
     u8g2_FirstPage(u8g2);
     do {
         u8g2_SetFont(u8g2, u8g2_font_ncenB08_tr);
         int y = 10;
-        for (int i = 0; currentMenu[i].name != NULL; i++) {
-            if (i == currentIndex) {
-                u8g2_DrawStr(u8g2, 0, y, "> ");
-                u8g2_DrawStr(u8g2, 10, y, currentMenu[i].name);
+        for (int i = 0; menuContext.currentMenu[i].name != NULL; i++) {
+            if (i == menuContext.currentIndex) {
+                // 添加框选效果，增强为填充背景色
+                u8g2_DrawBox(u8g2, 0, y - 8, u8g2_GetStrWidth(u8g2, menuContext.currentMenu[i].name) + 16, 10);
+                u8g2_SetDrawColor(u8g2, 0); // 设置文字颜色为反色
+                u8g2_DrawStr(u8g2, 10, y, menuContext.currentMenu[i].name);
+                u8g2_SetDrawColor(u8g2, 1); // 恢复默认绘制颜色
             } else {
-                u8g2_DrawStr(u8g2, 10, y, currentMenu[i].name);
+                u8g2_DrawStr(u8g2, 10, y, menuContext.currentMenu[i].name);
             }
             y += 10;
         }
     } while (u8g2_NextPage(u8g2));
 }
 
-// 处理菜单事件
+// 修改处理菜单事件函数
 void handleMenuEvent(u8g2_t *u8g2, uint8_t event) {
+    int menuCount = 0;
+    while (menuContext.currentMenu[menuCount].name != NULL) {
+        menuCount++;
+    }
     switch (event) {
         case U8X8_MSG_GPIO_MENU_UP:
-            if (currentIndex > 0) {
-                currentIndex--;
-            }
+            menuContext.currentIndex = (menuContext.currentIndex - 1 + menuCount) % menuCount;
             break;
         case U8X8_MSG_GPIO_MENU_DOWN:
-            if (currentMenu[currentIndex + 1].name != NULL) {
-                currentIndex++;
-            }
+            menuContext.currentIndex = (menuContext.currentIndex + 1) % menuCount;
             break;
         case U8X8_MSG_GPIO_MENU_SELECT:
-            if (currentMenu[currentIndex].submenu != NULL) {
-                currentMenu = currentMenu[currentIndex].submenu;
-                currentIndex = 0;
-            } else if (currentMenu[currentIndex].action != NULL) {
-                currentMenu[currentIndex].action();
+            if (menuContext.currentMenu[menuContext.currentIndex].submenu != NULL) {
+                menuContext.parentMenu = menuContext.currentMenu;
+                menuContext.currentMenu = menuContext.currentMenu[menuContext.currentIndex].submenu;
+                menuContext.currentIndex = 0;
+            } else if (menuContext.currentMenu[menuContext.currentIndex].action != NULL) {
+                menuContext.currentMenu[menuContext.currentIndex].action();
             }
             break;
         case U8X8_MSG_GPIO_MENU_HOME:
-            currentMenu = mainMenu;
-            currentIndex = 0;
+            menuContext.currentMenu = mainMenu;
+            menuContext.parentMenu = NULL;
+            menuContext.currentIndex = 0;
             break;
     }
-    drawMenu(u8g2);
+    drawAnimatedMenu(u8g2);
 }
 
-// 主函数示例
+// 修改主函数示例
 void demo(u8g2_t *u8g2) {
-    drawMenu(u8g2);
+    drawAnimatedMenu(u8g2);
     while (1) {
         uint8_t event = u8x8_GetMenuEvent(u8g2_GetU8x8(u8g2));
         if (event != 0) {
             handleMenuEvent(u8g2, event);
+            printf("Event: %d\r\n", event);
+            HAL_Delay(100); // 添加延迟以减少闪烁
         }
     }
 }
--- 6_u8g2_oled/Core/Src/usart.c
+++ 6_u8g2_oled/Core/Src/usart.c
@@ -21,7 +21,40 @@
 #include "usart.h"
 
 /* USER CODE BEGIN 0 */
+#ifdef __GNUC__
+/* With GCC, small printf (option LD Linker->Libraries->Small printf
+   set to 'Yes') calls __io_putchar() */
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#define GETCHAR_PROTOTYPE int __io_getchar(void)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#define GETCHAR_PROTOTYPE int fgetc(FILE *f)
+#endif /* __GNUC__ */
 
+/**
+  * @brief  Retargets the C library printf function to the USART.
+  * @param  None
+  * @retval None
+  */
+PUTCHAR_PROTOTYPE
+{
+  /* Place your implementation of fputc here */
+  /* e.g. write a character to the USART1 and Loop until the end of transmission */
+  HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, 0xFFFF);
+
+  return ch;
+}
+
+GETCHAR_PROTOTYPE
+{
+	uint8_t ch = 0;
+	// Clear the Overrun flag just before receiving the first character
+	__HAL_UART_CLEAR_OREFLAG(&huart1);
+ 
+	HAL_UART_Receive(&huart1, (uint8_t *)&ch, 1, 0xFFFF);
+	//HAL_UART_Transmit(&huart3, (uint8_t *)&ch, 1, 0xFFFF);
+	return ch;
+}
 /* USER CODE END 0 */
 
 UART_HandleTypeDef huart1;
